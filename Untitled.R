44bf3a24d5d2a3b69cfd74a65298b128292289fe
stlp <jsbwhaleguy@gmail.com>
2024-02-17 16:41:12 -0800

    idea

---
+++ Untitled.R
@@ -0,0 +1,4 @@
+# use select() for both datasets
+# merge(data frameA,data frameB,by="Country")
+# drop_na(name_of_the_column), cause we are prob gonna have some na values, or we can just filter out NA
+# values later when we are actually creating the website
